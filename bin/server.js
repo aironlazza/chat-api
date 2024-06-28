0f1e96e6eeb97125fe1e46c05753e9b1ede58abb
unknown <airon-lazzaretti@educar.rs.gov.br>
2024-06-28 16:55:47 -0300

    teste get

---
+++ bin/server.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const app = require("../src/api");
+
+app.use((req,res,next)=>{
+    next();
+})
+console.log(process.env.PORT);
+let port = process.env.PORT || 3001;
+app.listen(port);
+console.log(`listening on ${port}`);
